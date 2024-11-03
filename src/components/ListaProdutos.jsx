4d183a1492d3e78fd194659def497be395b7c9a4
MilBe2007 <mileny.santos@estudante.ifms.edu.br>
2024-11-03 02:00:41 -0300

    03-11

--- src/components/ListaProdutos.jsx
+++ src/components/ListaProdutos.jsx
@@ -2,9 +2,9 @@ import styles from '../styles/listarProdutos.module.css'
 
 export default function ListaProdutos( { lista } ){
     return(
-        <ul className={styles.bloco}>
+        <ul className={styles.blocoLista}>
         {lista.map(produto => (
-            <li key={produto.id}>
+            <li key={produto.id} className={styles.card}>
                 <h2>{produto.title}</h2>
                 <p>{produto.description}</p>
                     <p>Preço: R${produto.price}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styles from '../styles/listarProdutos.module.css';
 
 export default function Home(){
 
@@ -19,10 +20,28 @@ export default function Home(){
 
     return (
         <>
-        <h1>Lista de Produtos</h1>
-        <ul>
+        <header className={styles.header}>
+            <a href="/logo" className={styles.link}>
+                        <img src="/src/assets/logo.svg" alt="logo" className={styles.logo} /> 
+                    </a>
+                <h1 className={styles.title}>Empresas MB</h1>
+                <nav className={styles.nav}>
+                    <a href="/cart" className={styles.link}>
+                        <img src="/src/assets/cart.svg" alt="Carrinho" className={styles.icone} /> 
+                    </a>
+                    <a href="/cart1" className={styles.link}>
+                        <img src="/src/assets/cart1.png" alt="Login" className={styles.icone} /> 
+                    </a>
+                </nav>
+        </header>
+
+        <div className={styles.cabecalho}>
+        <h1 className={styles.titulo}>Lista de Produtos</h1>
+        </div>
+        
+        <ul className={styles.blocoLista}>
             {produto.map(produto => (
-                <li key={produto.id}>
+                <li key={produto.id} className={styles.card}>
                     <h2>{produto.title}</h2>
                     <p>{produto.description}</p>
                     <p>Preço: R${produto.price}</p>
@@ -30,6 +49,17 @@ export default function Home(){
                 </li>
             ))}
         </ul>
+        
+        <footer className={styles.rodape}>
+                <nav>
+                    <ul> 
+                        <li>Termos de Serviço</li>
+                        <li>Política de Privacidade</li>
+                        <li>Contato</li>
+                    </ul>
+                </nav>
+                <p className={styles.fim}>&copy; 2024 Produtos MB. Todos os direitos reservados.</p>
+        </footer>
        </>
     );
 }
